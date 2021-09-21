43cbbdb2a1f72ed57d9df6154ecfac4cd9c3fc0a
Mihir Rabade <mihirrabade@gmail.com>
2021-09-21 21:48:25 +0530

    Added 1 Slash command

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 Generating Discord Time Tags made easy!
 
 Currently the Bot is in development.
-You may invite the bot via this [link](https://discord.com/api/oauth2/authorize?client_id=889853627383046174&permissions=277562313734&scope=bot)
+You may invite the bot via this [link](https://discord.com/api/oauth2/authorize?client_id=889853627383046174&permissions=277025410054&scope=bot%20applications.commands)
 
 Join my server - https://discord.gg/MPtE9zsBs5
+
+Bot Permissions -
+
+![Bot Permissions](https://i.imgur.com/5zSzGYD.png)
--- bot.py
+++ bot.py
@@ -3,13 +3,37 @@ import os
 import discord
 from dotenv import load_dotenv
 
+from discord_slash import SlashCommand
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
+slash=SlashCommand(client, sync_commands=True)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    print("Slasher ready!")
+
+@slash.slash(name="Latency", description="Shows Bot Latency")
+async def ping(ctx):
+    await ctx.send(f'Bot Speed - {round(client.latency * 1000)}ms ')
+    '''
+    embed = discord.Embed(
+    title=f"Avtar of {ctx.author.display_name}",
+    color=discord.Color.teal()
+    ).set_image(url=ctx.author.avatar_url)
+
+    await ctx.send(embed=embed)
+'''
 client.run(TOKEN)
