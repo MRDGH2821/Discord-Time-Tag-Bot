b39c28c141299cc093c527338725dead369ee6c0
Mihir Rabade <mihirrabade@gmail.com>
2021-09-22 16:27:03 +0530

    Migrating to Javascrip based bot

---
+++ bot.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+console.log(process.env.TOKEN);
+
+// Require the necessary discord.js classes
+const {
+	Client,
+	Intents
+} = require('discord.js');
+const {
+	token
+} = require('./config.json');
+
+// Create a new client instance
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS]
+});
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
