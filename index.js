89cffee312d67496fdd2d75e722c6d73cd33979a
Mihir Rabade <mihirrabade@gmail.com>
2021-10-07 20:38:54 +0530

    Added partials

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS], partials: ['CHANNEL', 'REACTION'] });
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
