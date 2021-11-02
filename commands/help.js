13eb28df3556770d005b29ce4490494e08ad3b28
Mihir Rabade <mihirrabade@gmail.com>
2021-11-02 18:33:24 +0530

    Files Beautified using Prettier

--- commands/help.js
+++ commands/help.js
@@ -1,52 +1,80 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-const { MessageActionRow, MessageButton } = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const { MessageActionRow, MessageButton } = require("discord.js");
 
 const helpEmbed = new MessageEmbed()
-	.setColor('#F1EFEF')
-	.setTitle('Help Section')
-	.setURL('https://discord.gg/MPtE9zsBs5')
-	.setDescription('The help section you get you started!\nThe bot takes in various inputs & generates epoch & time tag for you. The commands are self explanatory and here is a brief overview of the parameters.')
-	.addFields(
-		{ name: 'Year', value: 'Year input should be in `YYYY` format. For example `2021`. Other formats may confuse the bot & result in unexpected output.' },
-		{ name: 'Month', value: 'Month input should be in `MM` or `m` format. For example `9` or `09`. It supports integer format only & might throw error when wrong format used.' },
-		{ name: 'Day', value: 'Same input rules as Month. Do note that if the Day exceeds `30` or `31` for a particular month, it will not throw error. Instead time tag will be generated after computing excess days. For example Input is `2021 09 32` September has only **30 days** so the time tag will be generated for `2021 10 2` i.e. `October 2 2021`' },
-		{ name: 'Hours', value: 'Same input rules as month' },
-		{ name: 'Minutes', value: 'Same input rules as month' },
-		{ name: 'Meridien', value: 'Ante meridien or Post Meridien (`AM` or `PM`)' },
-		{ name: 'UTC', value: 'Takes value in UTC Offset. For example -> `+05:00` or `-06:30` or `+00:00`. It is syntax sensitive. That means, you have to explictly put sign (`+` or `-`) & the trailing zeros.' },
-		{ name: 'Bot still not working?', value: 'Please join my [server](https://discord.gg/MPtE9zsBs5) & elaborate how you encountered that problem. Incase you are running out of time you may click [here](https://hammertime.djdavid98.art/). You may also submit an issue at [Github Repository](https://github.com/MRDGH2821/Discord-Time-Tag-Bot/issues) \n\n*Btw this site is not affliated with bot developer in any way, it was linked here to reduce your wasted time.*' },
-	);
+  .setColor("#F1EFEF")
+  .setTitle("Help Section")
+  .setURL("https://discord.gg/MPtE9zsBs5")
+  .setDescription(
+    "The help section you get you started!\nThe bot takes in various inputs & generates epoch & time tag for you. The commands are self explanatory and here is a brief overview of the parameters."
+  )
+  .addFields(
+    {
+      name: "Year",
+      value:
+        "Year input should be in `YYYY` format. For example `2021`. Other formats may confuse the bot & result in unexpected output."
+    },
+    {
+      name: "Month",
+      value:
+        "Month input should be in `MM` or `m` format. For example `9` or `09`. It supports integer format only & might throw error when wrong format used."
+    },
+    {
+      name: "Day",
+      value:
+        "Same input rules as Month. Do note that if the Day exceeds `30` or `31` for a particular month, it will not throw error. Instead time tag will be generated after computing excess days. For example Input is `2021 09 32` September has only **30 days** so the time tag will be generated for `2021 10 2` i.e. `October 2 2021`"
+    },
+    { name: "Hours", value: "Same input rules as month" },
+    { name: "Minutes", value: "Same input rules as month" },
+    {
+      name: "Meridien",
+      value: "Ante meridien or Post Meridien (`AM` or `PM`)"
+    },
+    {
+      name: "UTC",
+      value:
+        "Takes value in UTC Offset. For example -> `+05:00` or `-06:30` or `+00:00`. It is syntax sensitive. That means, you have to explictly put sign (`+` or `-`) & the trailing zeros."
+    },
+    {
+      name: "Bot still not working?",
+      value:
+        "Please join my [server](https://discord.gg/MPtE9zsBs5) & elaborate how you encountered that problem. Incase you are running out of time you may click [here](https://hammertime.djdavid98.art/). You may also submit an issue at [Github Repository](https://github.com/MRDGH2821/Discord-Time-Tag-Bot/issues) \n\n*Btw this site is not affliated with bot developer in any way, it was linked here to reduce your wasted time.*"
+    }
+  );
 
 const row = new MessageActionRow()
-	.addComponents(
-		new MessageButton()
-			.setLabel('Join Support Server')
-			.setStyle('LINK')
-			.setURL('https://discord.gg/MPtE9zsBs5'))
-	.addComponents(
-		new MessageButton()
-			.setLabel('Submit an Issue at GitHub')
-			.setStyle('LINK')
-			.setURL('https://github.com/MRDGH2821/Discord-Time-Tag-Bot/issues'))
-	.addComponents(
-		new MessageButton()
-			.setLabel('Backup Time Tag Generator')
-			.setStyle('LINK')
-			.setURL('https://hammertime.djdavid98.art/'))
-	.addComponents(
-		new MessageButton()
-			.setLabel('GitHub Repository')
-			.setStyle('LINK')
-			.setURL('https://github.com/MRDGH2821/Discord-Time-Tag-Bot'),
-	);
+  .addComponents(
+    new MessageButton()
+      .setLabel("Join Support Server")
+      .setStyle("LINK")
+      .setURL("https://discord.gg/MPtE9zsBs5")
+  )
+  .addComponents(
+    new MessageButton()
+      .setLabel("Submit an Issue at GitHub")
+      .setStyle("LINK")
+      .setURL("https://github.com/MRDGH2821/Discord-Time-Tag-Bot/issues")
+  )
+  .addComponents(
+    new MessageButton()
+      .setLabel("Backup Time Tag Generator")
+      .setStyle("LINK")
+      .setURL("https://hammertime.djdavid98.art/")
+  )
+  .addComponents(
+    new MessageButton()
+      .setLabel("GitHub Repository")
+      .setStyle("LINK")
+      .setURL("https://github.com/MRDGH2821/Discord-Time-Tag-Bot")
+  );
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('help')
-		.setDescription('The help section to get you started!'),
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("The help section to get you started!"),
 
-	async execute(interaction) {
-		await interaction.reply({ embeds:[helpEmbed], components: [row] });
-	},
+  async execute(interaction) {
+    await interaction.reply({ embeds: [helpEmbed], components: [row] });
+  }
 };
--- commands/time_tag.js
+++ commands/time_tag.js
@@ -1,112 +1,120 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const dayjs = require('dayjs');
-const customParseFormat = require('dayjs/plugin/customParseFormat');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const dayjs = require("dayjs");
+const customParseFormat = require("dayjs/plugin/customParseFormat");
 dayjs.extend(customParseFormat);
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('time_tag')
-		.setDescription('Generates Time tag in your local TimeZone! Simple & Easy!')
-		.addIntegerOption(option =>
-			option.setName('year')
-				.setDescription('Enter Year in YYYY format')
-				.setRequired(true))
-		.addIntegerOption(option =>
-			option.setName('month')
-				.setDescription('Enter Month in MM format')
-				.setRequired(true)
-				.addChoice('1', 1)
-				.addChoice('2', 2)
-				.addChoice('3', 3)
-				.addChoice('4', 4)
-				.addChoice('5', 5)
-				.addChoice('6', 6)
-				.addChoice('7', 7)
-				.addChoice('8', 8)
-				.addChoice('9', 9)
-				.addChoice('10', 10)
-				.addChoice('11', 11)
-				.addChoice('12', 12),
-		)
-		.addIntegerOption(option =>
-			option.setName('day')
-				.setDescription('Enter Day in DD format')
-				.setRequired(true),
-		)
-		.addIntegerOption(option =>
-			option.setName('hours')
-				.setDescription('Enter Hours in 12-hour format')
-				.setRequired(true)
-				.addChoice('1', 1)
-				.addChoice('2', 2)
-				.addChoice('3', 3)
-				.addChoice('4', 4)
-				.addChoice('5', 5)
-				.addChoice('6', 6)
-				.addChoice('7', 7)
-				.addChoice('8', 8)
-				.addChoice('9', 9)
-				.addChoice('10', 10)
-				.addChoice('11', 11)
-				.addChoice('12', 12),
-		)
-		.addIntegerOption(option =>
-			option.setName('minutes')
-				.setDescription('Enter Minutes')
-				.setRequired(true),
-		).addStringOption(option =>
-			option.setName('meridiem')
-				.setDescription('Ante or Post meridiem (AM or PM)')
-				.setRequired(true)
-				.addChoice('am', 'am')
-				.addChoice('pm', 'pm'),
-		),
+  data: new SlashCommandBuilder()
+    .setName("time_tag")
+    .setDescription("Generates Time tag in your local TimeZone! Simple & Easy!")
+    .addIntegerOption(option =>
+      option
+        .setName("year")
+        .setDescription("Enter Year in YYYY format")
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName("month")
+        .setDescription("Enter Month in MM format")
+        .setRequired(true)
+        .addChoice("1", 1)
+        .addChoice("2", 2)
+        .addChoice("3", 3)
+        .addChoice("4", 4)
+        .addChoice("5", 5)
+        .addChoice("6", 6)
+        .addChoice("7", 7)
+        .addChoice("8", 8)
+        .addChoice("9", 9)
+        .addChoice("10", 10)
+        .addChoice("11", 11)
+        .addChoice("12", 12)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName("day")
+        .setDescription("Enter Day in DD format")
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName("hours")
+        .setDescription("Enter Hours in 12-hour format")
+        .setRequired(true)
+        .addChoice("1", 1)
+        .addChoice("2", 2)
+        .addChoice("3", 3)
+        .addChoice("4", 4)
+        .addChoice("5", 5)
+        .addChoice("6", 6)
+        .addChoice("7", 7)
+        .addChoice("8", 8)
+        .addChoice("9", 9)
+        .addChoice("10", 10)
+        .addChoice("11", 11)
+        .addChoice("12", 12)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName("minutes")
+        .setDescription("Enter Minutes")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName("meridiem")
+        .setDescription("Ante or Post meridiem (AM or PM)")
+        .setRequired(true)
+        .addChoice("am", "am")
+        .addChoice("pm", "pm")
+    ),
 
-	async execute(interaction) {
+  async execute(interaction) {
+    const year = interaction.options.getInteger("year");
+    const month = interaction.options.getInteger("month");
+    const day = interaction.options.getInteger("day");
+    const hour = interaction.options.getInteger("hours");
+    const min = interaction.options.getInteger("minutes");
+    const meridiem = interaction.options.getString("meridiem");
 
-		const year = interaction.options.getInteger('year');
-		const month = interaction.options.getInteger('month');
-		const day = interaction.options.getInteger('day');
-		const hour = interaction.options.getInteger('hours');
-		const min = interaction.options.getInteger('minutes');
-		const meridiem = interaction.options.getString('meridiem');
+    const daystr =
+      year + " " + month + " " + day + " " + hour + " " + min + " " + meridiem;
+    const epoch = dayjs(daystr, "YYYY M D HH m a").unix();
 
-		const daystr = year + ' ' + month + ' ' + day + ' ' + hour + ' ' + min + ' ' + meridiem;
-		const epoch = dayjs(daystr, 'YYYY M D HH m a').unix();
+    const tagOutput = {
+      color: "0xf1efef",
+      title: "Time Tag Generated!",
+      description: "Following are the inputs given!",
+      fields: [
+        {
+          name: "Time Epoch",
+          value: `\`${epoch}\``
+        },
+        {
+          name: "Time Tag",
+          value: `<t:${epoch}>`
+        },
+        {
+          name: "Date",
+          value: `${year}-${month}-${day}`
+        },
+        {
+          name: "Time",
+          value: `${hour}:${min} ${meridiem} (In your local TimeZone)`
+        }
+      ],
+      timestamp: new Date()
+    };
 
-		const tagOutput = {
-			color: '0xf1efef',
-			title: 'Time Tag Generated!',
-			description: 'Following are the inputs given!',
-			fields: [
-				{
-					name: 'Time Epoch',
-					value: `\`${epoch}\``,
-				},
-				{
-					name: 'Time Tag',
-					value: `<t:${epoch}>`,
-				},
-				{
-					name: 'Date',
-					value: `${year}-${month}-${day}`,
-				},
-				{
-					name: 'Time',
-					value: `${hour}:${min} ${meridiem} (In your local TimeZone)`,
-				},
-			],
-			timestamp: new Date(),
-		};
-
-		try {
-			await interaction.reply({ embeds:[tagOutput] });
-			await interaction.followUp(`\`<t:${epoch}>\``);
-		}
-		catch (error) {
-			console.error(error);
-			return interaction.reply(`Uhhh, sorry an error occured. Please use /help command & reach out bot developer with error screenshot.\nError dump: ${error}`);
-
-		}
-	},
+    try {
+      await interaction.reply({ embeds: [tagOutput] });
+      await interaction.followUp(`\`<t:${epoch}>\``);
+    } catch (error) {
+      console.error(error);
+      return interaction.reply(
+        `Uhhh, sorry an error occured. Please use /help command & reach out bot developer with error screenshot.\nError dump: ${error}`
+      );
+    }
+  }
 };
