c2cfc860f5e8334aa50924f2cf096339af44ab27
MRDGH2821 <mihirrabade@gmail.com>
2021-12-05 17:06:04 +0530

    Now Shows time when executed

--- commands/user_info.js
+++ commands/user_info.js
@@ -1,10 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { time } = require('@discordjs/builders');
+const date = new Date();
+
+const timeString = time(date);
+const relative = time(date, 'R');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user_info')
 		.setDescription('Display info about yourself.'),
 	async execute(interaction) {
-		return interaction.reply(`Your username: ${interaction.user.username}\nYour ID: ${interaction.user.id}`);
+		return interaction.reply(
+			`Your username: ${interaction.user.username}\nYour ID: ${interaction.user.id}\nThe time on which this command was executed: ${timeString}\ni.e. ${relative}`,
+		);
 	},
 };
