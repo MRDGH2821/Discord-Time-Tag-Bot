91fc54af742d6c9b2aace9ede5e47a177b95728d
MRDGH2821 <mihirrabade@gmail.com>
2021-12-05 17:10:55 +0530

    Fixed working of User Info command

--- commands/user_info.js
+++ commands/user_info.js
@@ -1,17 +1,15 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { time } = require('@discordjs/builders');
-const date = new Date();
-
-const timeString = time(date);
-const relative = time(date, 'R');
+const { SlashCommandBuilder, time } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user_info')
 		.setDescription('Display info about yourself.'),
 	async execute(interaction) {
+		const date = new Date();
+		const timeString = time(date);
+		const relative = time(date, 'R');
 		return interaction.reply(
-			`Your username: ${interaction.user.username}\nYour ID: ${interaction.user.id}\nThe time on which this command was executed: ${timeString}\ni.e. ${relative}`,
+			`Your username: ${interaction.user.tag}\nYour ID: ${interaction.user.id}\nThe time on which this command was executed: ${timeString}\ni.e. ${relative}`,
 		);
 	},
 };
