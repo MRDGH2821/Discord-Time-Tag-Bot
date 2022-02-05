5d6ef2a87a894e78bd39412442613c3758783d6f
MRDGH2821 <mihirrabade@gmail.com>
2022-02-05 17:21:43 +0530

    fix: update ecosystem file

--- time-tag.config.js
+++ time-tag.config.js
@@ -1,9 +1,21 @@
 module.exports = {
   apps: [
     {
+      env_dev: {
+        NODE_ENV: 'dev'
+      },
+      env_prod: {
+        NODE_ENV: 'prod'
+      },
       name: 'Time Tag Bot',
       script: './index.js',
       watch: true
+    },
+    {
+      autorestart: false,
+      name: 'TTG Deploy',
+      script: './deploy-check.js',
+      watch: false
     }
   ]
 };
