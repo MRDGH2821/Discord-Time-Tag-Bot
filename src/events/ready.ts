7249179b4010ccb03fd3d1970e82d0106a53962a
MRDGH2821 <mihirrabade@gmail.com>
2022-11-08 15:13:33 +0530

    feat: display activities & commands in log

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,58 +1,61 @@
 import { GatewayClientEvents } from 'detritus-client';
-import { ClientEvents } from 'detritus-client/lib/constants';
+import { ActivityTypes, ClientEvents } from 'detritus-client/lib/constants';
 import BotEvent from '../lib/BotEvent';
+import EnvConfig from '../lib/EnvConfig';
 
 const { version } = require('../../package.json');
 
 export default new BotEvent({
   event: ClientEvents.GATEWAY_READY,
-  listener(args) {
-    console.log(args);
-    const client: GatewayClientEvents.ClusterEvent['shard'] = args.shard;
+  async listener(args) {
+    //  console.log(args);
+    const shardClient: GatewayClientEvents.ClusterEvent['shard'] = args.shard;
     const gatewayRaw: GatewayClientEvents.GatewayReady['raw'] = args.raw;
     console.log(`Ready! Logged in as ${gatewayRaw.user.username}#${gatewayRaw.user.discriminator}`);
+    console.log(
+      'Guild commands: ',
+      await shardClient.rest.fetchApplicationGuildCommands(
+        shardClient.applicationId,
+        EnvConfig.guildId,
+      ),
+    );
 
+    console.log(
+      'Global commands: ',
+      await shardClient.rest.fetchApplicationCommands(shardClient.applicationId),
+    );
     const activities = [
       {
-        msg: `bot version ${version}`,
-        obj: {
-          type: 'PLAYING',
-        },
+        name: `bot version ${version}`,
+        type: ActivityTypes.PLAYING,
       },
       {
-        msg: `/help in ${client.guilds.cache.size} server(s)`,
-        obj: {
-          type: 'LISTENING',
-        },
+        name: `/help in ${shardClient.guilds.cache.size} server(s)`,
+        type: ActivityTypes.LISTENING,
       },
       {
-        msg: 'a new event happen',
-        obj: {
-          type: 'WATCHING',
-        },
+        name: 'a new event happen',
+        type: ActivityTypes.WATCHING,
       },
       {
-        msg: `an event in one of ${client.guilds.cache.size} server(s)`,
-        obj: {
-          type: 'PLAYING',
-        },
+        name: `an event in one of ${shardClient.guilds.cache.size} server(s)`,
+        type: ActivityTypes.PLAYING,
       },
       {
-        msg: 'an event manager',
-        obj: {
-          type: 'LISTENING',
-        },
+        name: 'an event manager',
+        type: ActivityTypes.LISTENING,
       },
     ];
     const delay = 30000;
+    shardClient.user?.presence?.activity = activities[0];
     // console.log(activities);
     /*
-    client.user.setActivity(activities[0].msg, activities[0].obj);
-    // console.log('Activities:\n', activities[2].msg, activities[2].obj);
+    client.user.setActivity(activities[0].name, activities[0].obj);
+    // console.log('Activities:\n', activities[2].name, activities[2].obj);
     setInterval(() => {
       const act = activities[Math.floor(Math.random() * activities.length)];
       console.log(act);
-      client.user.setActivity(act.msg, act.obj);
+      client.user.setActivity(act.name, act.obj);
     }, delay);
     */
   },
