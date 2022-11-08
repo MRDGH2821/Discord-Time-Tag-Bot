40a900e06181fccf75d4621c23fdec7ff67a0d7f
MRDGH2821 <mihirrabade@gmail.com>
2022-11-08 15:15:48 +0530

    chore: import events

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { ClusterClient, InteractionCommandClient } from 'detritus-client';
+import interactionCreate from './events/interactionCreate';
+import ready from './events/ready';
 import EnvConfig from './lib/EnvConfig';
 
 (async () => {
@@ -7,10 +9,20 @@ import EnvConfig from './lib/EnvConfig';
       channels: { enabled: true, limit: 10 },
     },
   });
+  clusterBot.on(ready.event, ready.listener);
+  clusterBot.on(interactionCreate.event, interactionCreate.listener);
 
   await clusterBot.run();
 
   const interactionBot = new InteractionCommandClient(clusterBot);
-  interactionBot.addMultipleIn('./commands/');
-  await interactionBot.run().then(() => {});
+  /*
+  await interactionBot.rest.bulkOverwriteApplicationCommands(EnvConfig.clientId, []);
+  await interactionBot.rest.bulkOverwriteApplicationGuildCommands(
+    EnvConfig.clientId,
+    EnvConfig.guildId,
+    [],
+  );
+  */
+  await interactionBot.addMultipleIn('./commands/');
+  await interactionBot.run();
 })().catch(console.error);
