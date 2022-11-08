ab79dae16c4836aded3b46f7afa7ee9022d63e58
MRDGH2821 <mihirrabade@gmail.com>
2022-11-08 18:18:32 +0530

    chore: move search function to Utilities
    Change default value to 0

--- src/lib/ReusableComponents.ts
+++ src/lib/ReusableComponents.ts
@@ -1,12 +1,11 @@
-import { getTimeZones } from '@vvo/tzdb';
 import {
   ApplicationCommandOptionTypes,
   MessageComponentButtonStyles,
 } from 'detritus-client/lib/constants';
 import { InteractionCommandOptionOptions } from 'detritus-client/lib/interaction';
 import { ComponentActionRow } from 'detritus-client/lib/utils';
+import { encodeInvalid, offSetMinutesToClock, searchTZ } from './Utilities';
 
-export const timeZones = getTimeZones({ includeUtc: true });
 export const bkpRow = new ComponentActionRow().addButton({
   label: 'Backup Time Tag Generator',
   style: MessageComponentButtonStyles.LINK,
@@ -29,38 +28,28 @@ export const utcOption: InteractionCommandOptionOptions = {
   name: 'utc',
   description: 'Enter timezone in UTC (default Etc/UTC i.e. +00:00)',
   type: ApplicationCommandOptionTypes.STRING,
-  default: 'Etc/UTC',
+  default: '0',
   async onAutoComplete(ctx) {
     const input = ctx.value.toLowerCase();
     console.log({ input });
-    const foundTZ = timeZones
-      .filter((timeZone) => Object.values(timeZone)
-        .flat()
-        .some((val) => val.includes(input)))
-      .slice(0, 25);
+    const foundTZ = searchTZ(input);
 
-    const parsedTZ = foundTZ.map((timeZone) => ({
-      name: `${timeZone.rawFormat} ${timeZone.abbreviation}`,
-      value: timeZone.name,
-    }));
+    const parsedTZ = foundTZ
+      .map((timeZone) => ({
+        name: encodeInvalid(
+          `${timeZone.name} ${timeZone.abbreviation} ${offSetMinutesToClock(
+            timeZone.rawOffsetInMinutes,
+          )}`,
+        ),
+        value: timeZone.rawOffsetInMinutes.toString(),
+      }))
+      .slice(0, 23);
+
     parsedTZ.push({
       name: '+00:00 Coordinated Universal Time (UTC)',
-      value: 'Etc/UTC',
+      value: '0',
     });
     console.log({ foundTZ, parsedTZ });
     return parsedTZ;
   },
 };
-
-/*
-return {
-          name: '+00:00 Coordinated Universal Time (UTC)',
-          value: 'Etc/UTC',
-        };
-
-         return {
-            name: `${timeZone.rawFormat} ${timeZone.abbreviation}`,
-            value: timeZone.name,
-          };
-
-        */
